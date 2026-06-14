d12755f8be0d629526ff7ac0c23f909985d5efc8
Nura Husein <nurahusein47@gmail.com>
2026-06-14 16:02:10 +0300

    Add files via upload

---
+++ simple caluletar (1).py
@@ -0,0 +1,35 @@
+while True:
+
+    print("\n=== SIMPLE CALCULATOR ===")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "5":
+        print("Calculator Closed.")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == "1":
+        print("Result =", num1 + num2)
+
+    elif choice == "2":
+        print("Result =", num1 - num2)
+
+    elif choice == "3":
+        print("Result =", num1 * num2)
+
+    elif choice == "4":
+        if num2 != 0:
+            print("Result =", num1 / num2)
+        else:
+            print("Error! Division by zero is not allowed.")
+
+    else:
+        print("Invalid choice.")
